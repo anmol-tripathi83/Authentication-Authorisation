098e869c0951566e66124a54aceb75862f80bc93
anmol-tripathi83 <anmoltripathi8303@gmail.com>
2024-11-16 04:15:25 +0530

    Login router completed and token generated

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,4 +1,6 @@
 const userModel = require("../Models/userModel");
+// importing and jwtwebtoken(after installing it by "npm i jwtwebtoken" command)
+const jwt = require("jsonwebtoken");
 
 const handleUserSignup = (req,res) =>{
     try{
@@ -14,4 +16,29 @@ catch(err){
 }
 }
 
-module.exports = {handleUserSignup};
+const handleUserLogin = (req,res) =>{
+    let {email} = req.body;    // descructuring of object
+    try{
+        userModel.find({email:email}).then((response) =>{     // response we are getting here is in form of array
+            if(response.length > 0){
+                jwt.sign(req.body, process.env.SECRET_KEY,(err,token) =>{
+                    if(err){
+                        res.json({"Message":"This is wrong","err":err}).status(500);
+                    }
+                    else{
+                        res.json({
+                            "Message":"Login Successfull",
+                            "data":req.body,         // to see which user's login has been done
+                            "token":token
+                        }).status(200);
+                    }
+                });                                                                       // {expiresIn:'1d'} - In third argument but currently I am doing this this will generate the token and expires after 1 day        
+            }
+        });
+    }catch(err){
+        res.json({"Message":"This is wrong","err":err}).status(500);
+    }
+}
+
+module.exports = {handleUserSignup, handleUserLogin};
+
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,7 +1,10 @@
 var express = require("express");
-const { handleUserSignup } = require("../Controller/UserController");
+const { handleUserSignup, handleUserLogin } = require("../Controller/UserController");
 var router = express.Router();
 
+// http://localhost:5002/api/v1/user/signup
 router.post('/signup',handleUserSignup);
+// http://localhost:5002/api/v1/user/login
+router.post('/login',handleUserLogin);      // now jwt token came into picture
 
 module.exports = router;
